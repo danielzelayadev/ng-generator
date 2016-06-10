802b44af58ca96782f3b81a3aea95af6e7985a61
wupa9 <wupaz9@gmail.com>
2016-06-10 03:58:21 -0600

    Implement service function for selecting a directory

--- dev/app/services/GenUtils/gen-utils.service.js
+++ dev/app/services/GenUtils/gen-utils.service.js
@@ -3,7 +3,9 @@ genUtils.$injects = [];
 function genUtils () {
 	const service = {
 		addInputTextToList: addInputTextToList,
-		removeListItem: removeListItem
+		removeListItem: removeListItem,
+		openFileBrowser: openFileBrowser,
+		toStandardFileName: toStandardFileName
 	};
 
 	return service;
@@ -18,6 +20,29 @@ function genUtils () {
 
 	function removeListItem (index, list) {
 		list.splice(index, 1);
+	}
+
+	function openFileBrowser () {
+		return dialog
+			.showOpenDialog({properties: ['openFile', 'openDirectory']});
+	}
+
+	function toStandardFileName (str) {
+		if (!str || str.length == 0) return;
+
+		let currLength = str.length;
+
+		for (let i = 0; i < currLength; i++) {
+			let c = str[i];
+
+			if (i > 0 && i < (currLength - 1) && c !== '-' && c === c.toUpperCase()) {
+				str = str.slice(0, i) + '-' + str.slice(i);
+				i++;
+				currLength++;
+			}
+		}
+
+		return str.toLowerCase();
 	}
 
 }
