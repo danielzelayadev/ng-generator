ea92bbbc5a798ab1a2ee66063382b3eeb8554fe4
wupa9 <wupaz9@gmail.com>
2016-06-12 03:30:49 -0600

    Add atomic boilerplate generator functions

--- dev/app/services/Generator/generator.service.js
+++ dev/app/services/Generator/generator.service.js
@@ -8,7 +8,46 @@ function generator () {
 	return service;
 
 	function createApp (appData) {
+		
 	}
+
+	function createAppBootstrapper (dir, requires) {
+
+	}
+
+	function createModule (dir, moduleData, type) { 
+	//moduleName, dependencies, moduleVars, moduleRegistries
+
+	}
+
+	function createConfig (dir, dependencies, type) {
+
+	}
+
+	function createRun (dir, dependencies) {
+
+	}
+
+	function createTemplate (dir, name) {
+
+	}
+
+	function createStylsheet (dir, name) {
+
+	}
+
+	function createController (dir, name, dependencies) {
+
+	}
+
+	function createService (dir, name, dependencies) {
+
+	}
+
+	function createDirective (dir, name, dependencies) {
+
+	}
+
 }
 
 module.exports = { name: 'generator', srvc: generator };
