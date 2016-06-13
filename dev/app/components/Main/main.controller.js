54ba8584a39392e0f5bd2d2fe4e2b5209ea85626
wupa9 <wupaz9@gmail.com>
2016-06-13 02:11:08 -0600

    Change nav options

--- dev/app/components/Main/main.controller.js
+++ dev/app/components/Main/main.controller.js
@@ -1,21 +1,34 @@
-MainController.$inject = [ '$scope', '$state' ];
+MainController.$inject = [ '$scope', 'genUtils', '$rootScope' ];
 
-function MainController (scope, state) {
+function MainController (scope, genUtils, rootScope) {
 	const vm = this;
 
 	vm.navOptions = [
 		{ name: 'New Application', sref: 'main.new-app' },
-		{ name: 'New Component', sref: 'main.new-component' },
-		{ name: 'New Service', sref: 'main.new-service' },
-		{ name: 'New Shared Module', sref: 'main.new-shared' }
+		{ name: 'Open Application', sref: genUtils.currentState() },
+		{ name: 'Close', sref: 'main.welcome' }
 	];
 	vm.openMenu = openMenu;
+	vm.openApp  = openApp;
 
 	function openMenu (mdOpenMenu, event) {
 		mdOpenMenu(event);
 	}
 
-	state.go('main.new-app');
+	function openApp () {
+		try {
+			const app = genUtils.openApp();
+			// genUtils.goTo('main.open-app', app);
+		} catch (err) {
+			genUtils.toast(err, 5000);
+		}
+	}
+
+	rootScope.$on('$stateChangeStart', event => {
+		vm.navOptions[2].disabled = genUtils.currentState() !== 'main.welcome';
+	});
+	
+	genUtils.goTo('main.welcome');
 }
 
 module.exports = { name: 'MainController', ctrl: MainController };
